5ce799fcbc4cdd32d8e44d2eb940a9ab437945e0
Ky Nguyen <nguyentruongky33@gmail.com>
2021-06-27 06:57:43 +0700

    Do minor changes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
-const functions = require('firebase-functions')
-const express = require('express')
-const cors = require('cors')
-const stripe = require('stripe')('sk_test_gUlPGbe57OnirEOH8xb6wHiS00VjQexdTh')
-
-// API
+// prettier-ignore
+const functions = require("firebase-functions")
+// prettier-ignore
+const express = require("express")
+// prettier-ignore
+const cors = require("cors")
+// prettier-ignore
+const stripe = require("stripe")("sk_test_gUlPGbe57OnirEOH8xb6wHiS00VjQexdTh")
 
 // App config
 const app = express()
@@ -13,24 +15,29 @@ app.use(cors({ origin: true }))
 app.use(express.json())
 
 // API routes
-app.get('/', (req, res) => {
-    res.status(200).send('Hello testing')
+// prettier-ignore
+app.get("/", (req, res) => {
+    // prettier-ignore
+    res.status(200).send("Hello testing")
 })
 
-app.post('/payments/create', async (req, res) => {
+// prettier-ignore
+app.post("/payments/create", (req, res) => {
     const total = req.query.total
-    console.log('payment request total', total)
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: 'usd',
-    })
-
-    res.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    })
+    stripe.paymentIntents
+        .create({
+            amount: total,
+            // prettier-ignore
+            currency: "usd",
+        })
+        .then((paymentIntent) => {
+            res.status(201).send({
+                clientSecret: paymentIntent.client_secret,
+            })
+        })
 })
 
 // Listener
 exports.api = functions.https.onRequest(app)
 
-// http://localhost:5001/clone-7dbdc/us-central1/api
+// API URL: http://localhost:5001/clone-7dbdc/us-central1/api
--- src/Home.js
+++ src/Home.js
@@ -31,35 +31,42 @@ function Home() {
 
                 <div className="home__row">
                     <Product
-                        id="1"
-                        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
-                        price={19.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-                        rating={3}
+                        id="3"
+                        title="Hisense 40-Inch 40H5500F Class H55 Series Android Smart TV with Voice Remote (2020 Model)"
+                        price={248.0}
+                        image="https://images-na.ssl-images-amazon.com/images/I/61lJa-K8CqL._AC_SL1001_.jpg"
+                        rating={5}
                     />
                     <Product
-                        id="2"
-                        title="HP Chromebook 11-inch Laptop - Up to 15 Hour Battery Life - MediaTek - MT8183 - 4 GB RAM - 32 GB eMMC Storage - 11.6-inch HD Display - with Chrome OS - (11a-na0021nr, 2020 Model, Snow White)"
-                        image="https://m.media-amazon.com/images/I/81EY2GIfktL._AC_UY436_FMwebp_QL65_.jpg"
-                        price={539}
+                        id="4"
+                        title="SanDisk 1TB Extreme Portable SSD - Up to 1050MB/s - USB-C, USB 3.2 Gen 2 - External Solid State Drive - SDSSDE61-1T00-G25"
+                        image="https://images-na.ssl-images-amazon.com/images/I/712%2Bjjz3lyL._AC_SL1500_.jpg"
+                        price={154.38}
                         rating={4}
                     />
                     <Product
-                        id="1"
-                        title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
-                        price={19.99}
-                        image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+                        id="5"
+                        title="Apple iPhone 11 Pro, 64GB, Space Gray - Unlocked (Renewed Premium)"
+                        price={720.0}
+                        image="https://images-na.ssl-images-amazon.com/images/I/81LmL94PUvS._AC_SL1500_.jpg"
                         rating={3}
                     />
                 </div>
 
                 <div className="home__row">
                     <Product
-                        id="2"
-                        title="HP Chromebook 11-inch Laptop - Up to 15 Hour Battery Life - MediaTek - MT8183 - 4 GB RAM - 32 GB eMMC Storage - 11.6-inch HD Display - with Chrome OS - (11a-na0021nr, 2020 Model, Snow White)"
-                        image="https://m.media-amazon.com/images/I/81EY2GIfktL._AC_UY436_FMwebp_QL65_.jpg"
-                        price={539}
-                        rating={4}
+                        id="6"
+                        title="Apple MacBook Pro with Apple M1 Chip (13-inch, 16GB RAM, 256GB SSD Storage) - Space Gray (Latest Model) Z11B000E3"
+                        image="https://images-na.ssl-images-amazon.com/images/I/71an9eiBxpL._AC_SL1500_.jpg"
+                        price={1449.99}
+                        rating={5}
+                    />
+                    <Product
+                        id="7"
+                        title="LG Gram 16Z90P - 16 WGXGA (2560x1600) Ultra-Lightweight Laptop, Intel evo with 11th gen CORE i7 1165G7 CPU , 16GB RAM, 256GB SSD, Alexa Built-in, 19.5 Hours Battery, Thunderbolt 4, Black - 2021"
+                        image="https://images-na.ssl-images-amazon.com/images/I/81pKId7SSYS._AC_SL1500_.jpg"
+                        price={1349.99}
+                        rating={5}
                     />
                 </div>
             </div>
